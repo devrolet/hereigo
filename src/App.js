b18ce57acbae2172bf977151ead2caf4d2a7bad7
Chet Hill <chetdhill@gmail.com>
2023-01-28 15:20:30 -0600

    Create MainHeader and MainNavigation Components

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import './App.css';
 import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-  return <Router>
-    <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Route path="/places/new" exact>
-        <NewPlace />
-      </Route>
-      <Redirect to="/" />
-    </Switch>
-  </Router>
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
