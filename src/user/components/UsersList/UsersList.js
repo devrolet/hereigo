b87ac28906207c06d4dc2f2856b56e04a4b61534
Chet Hill <chetdhill@gmail.com>
2023-01-28 14:51:39 -0600

    Add Card to UserList if no users found

--- src/user/components/UsersList/UsersList.js
+++ src/user/components/UsersList/UsersList.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import UsersItem from "../UsersItem/UsersItem";
+import Card from "../../../shared/components/UIElements/Card/Card";
 import './UsersList.css';
 
 const UsersList = props => {
     if(props.items.length === 0) {
         return (
             <div className="center">
-                <h2>No Users Found.</h2>
+                <Card>
+                    <h2>No Users Found.</h2>
+                </Card>
             </div>
         );
     }
