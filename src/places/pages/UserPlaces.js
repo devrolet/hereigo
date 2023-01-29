08df78074ae0f53287772367d3ba6b67a5e15fd4
Chet Hill <chetdhill@gmail.com>
2023-01-29 12:24:13 -0600

    Filter UserPlaces by UserId

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,36 +1,39 @@
-import React from "react";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
-const UserPlaces = () => {
-
-  const DUMMY_PLACES = [
-    {
-      id: 'p1',
-      title: 'Eiffel Tower',
-      description: 'The most iconic structure in the City of Love',
-      imageUrl: 'https://www.pexels.com/photo/eiffel-tower-at-night-2348811/',
-      address: 'Champ de Mars, 5 Av. Anatole France, 75007 Paris, France',
-      location: {
-        lat: 48.8583694,
-        lng: 2.2841826
-      },
-      creator: '01'
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Eiffel Tower',
+    description: 'The most iconic structure in the City of Love',
+    imageUrl: 'https://www.pexels.com/photo/eiffel-tower-at-night-2348811/',
+    address: 'Champ de Mars, 5 Av. Anatole France, 75007 Paris, France',
+    location: {
+      lat: 48.8583694,
+      lng: 2.2841826
     },
-    {
-      id: 'p2',
-      title: 'Eiffel Tower',
-      description: 'The most iconic structure in the City of Love',
-      imageUrl: 'https://www.pexels.com/photo/eiffel-tower-at-night-2348811/',
-      address: 'Champ de Mars, 5 Av. Anatole France, 75007 Paris, France',
-      location: {
-        lat: 48.8583694,
-        lng: 2.2841826
-      },
-      creator: '02'
-    }
-  ]
-  return <PlaceList items={DUMMY_PLACES} />
+    creator: '01'
+  },
+  {
+    id: 'p2',
+    title: 'Eiffel Tower',
+    description: 'The most iconic structure in the City of Love',
+    imageUrl: 'https://www.pexels.com/photo/eiffel-tower-at-night-2348811/',
+    address: 'Champ de Mars, 5 Av. Anatole France, 75007 Paris, France',
+    location: {
+      lat: 48.8583694,
+      lng: 2.2841826
+    },
+    creator: '02'
+  }
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />
 };
 
 export default UserPlaces;
